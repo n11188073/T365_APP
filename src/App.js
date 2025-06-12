f6143f47fea24e49b0f99f66989cdea75e203835
Bernie <n11188073@qut.edu.au>
2025-06-12 18:03:54 +1000

    Added nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { fetchWeather } from './api/fetchWeather';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faPlus, faUser, faCalendar, faComment } from '@fortawesome/free-solid-svg-icons';
+
 
 const App = () => {
     const [query, setQuery] = useState('');
@@ -15,33 +18,52 @@ const App = () => {
     }
 
     return (
-    <div className="main-container">
-        <input
-            type="text"
-            className="search"
-            placeholder="Search..."
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            onKeyPress={search}
-        />
-        {weather.main && (
-            <div className="city">
-                <h2 className="city-name">
-                    <span>{weather.name}</span>
-                    <sup>{weather.sys.country}</sup>
-                    <div className="city-temp">
-                        {Math.round(weather.main.temp)}
-                        <sup>&deg;C</sup>
-                    </div>
-                    <div className="info">
-                        <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="weather icon" />
-                        <p>{weather.weather[0].description}</p>
-                    </div>
-                </h2>
-            </div>
-        )}
-    </div>
+    <>
+  <div className="main-container">
+    <input
+      type="text"
+      className="search"
+      placeholder="Search...                        🔍"
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      onKeyPress={search}
+    />
+    {weather.main && (
+      <div className="city">
+        <h2 className="city-name">
+          <span>{weather.name}</span>
+          <sup>{weather.sys.country}</sup>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt="weather icon"
+              className="city-icon"
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </h2>
+      </div>
+    )}
+  </div>
+
+  {/* Bottom Nav Bar */}
+<div className="bottom-nav">
+  <div className="nav-icon"><FontAwesomeIcon icon={faHome} /></div>
+  <div className="nav-icon"><FontAwesomeIcon icon={faComment} /></div>
+  <div className="nav-icon"><FontAwesomeIcon icon={faPlus} /></div>
+  <div className="nav-icon"><FontAwesomeIcon icon={faCalendar} /></div>
+  <div className="nav-icon"><FontAwesomeIcon icon={faUser} /></div>
+</div>
+</>
+
+
   );
 };
 
 export default App;
+
+
