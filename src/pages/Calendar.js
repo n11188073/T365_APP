37465c2d425dac08b293528a75f11f62de8ddf7c
Phoebe <phoebehoo123@gmail.com>
2025-09-11 12:18:08 +1000

    UI Progress with itinerary

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const Calendar = () => {
   return (
     <div className="page">
-      <h1>Calendar</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin convallis velit non lectus imperdiet.</p>
+      <div className="page-header">
+        <h1>Itineraries</h1>
+        <FontAwesomeIcon icon={faPlus} className="header-icon" />
+      </div>
+
+      <p>
+        Itinary Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+        convallis velit non lectus imperdiet.
+      </p>
+
+      {/* Card */}
+      <div className="card">
+        <h2>Testing Card</h2>
+        <p>Itinary Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+        convallis velit non lectus imperdiet.</p>
+      </div>
     </div>
   );
 };
